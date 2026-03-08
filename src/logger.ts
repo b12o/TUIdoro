33ea44bfd9d28f70d9a714c7709a023a37a4dca9
b12o <barny@edubio.de>
2026-03-08 10:51:52 +0100

    log to file instead of stdout since TUI requires window

--- src/logger.ts
+++ src/logger.ts
@@ -1,3 +1,5 @@
+import { appendFileSync } from "fs";
+
 const levels = {
   debug: 0,
   info: 1,
@@ -6,16 +8,20 @@ const levels = {
 };
 
 type Level = keyof typeof levels;
-
 const currentLevel: Level = (process.env.LOG_LEVEL as Level) || "info";
+
+function logToFile(msg: string) {
+  // tuidoro is located in project root.
+  appendFileSync("tuidoro.log", `${msg}\n`);
+}
 
 export const logger = {
   debug: (msg: string) =>
-    levels[currentLevel] <= levels.debug && console.log(`[DEBUG] ${msg}`),
+    levels[currentLevel] <= levels.debug && logToFile(`[DEBUG] ${msg}`),
   info: (msg: string) =>
-    levels[currentLevel] <= levels.info && console.log(`[INFO] ${msg}`),
+    levels[currentLevel] <= levels.info && logToFile(`[INFO] ${msg}`),
   warn: (msg: string) =>
-    levels[currentLevel] <= levels.warn && console.log(`[WARN] ${msg}`),
+    levels[currentLevel] <= levels.warn && logToFile(`[WARN] ${msg}`),
   error: (msg: string) =>
-    levels[currentLevel] <= levels.error && console.log(`[ERROR] ${msg}`),
+    levels[currentLevel] <= levels.error && logToFile(`[ERROR] ${msg}`),
 };
